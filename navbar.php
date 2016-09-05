ac9cb0e33c07b5ffa28fa0a3696fc7b58fb50d89
csf30816 <elijahporter@zoho.com>
2016-09-05 11:59:36 -0500

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,11 +1,19 @@
 	<div id="head_bar">
             <ul class="navbar">
-                <a href="/">
+                <a href="/" id="/" class="nav_button">
                     <li>Home</li>
                 </a>
-                <a href="/signup.php">
+                <a href="/signup.php" id="/signup.php" class="nav_button">
                     <li>Signup</li>
                 </a>
                 <span>&nbsp;</span>
             </ul>
         </div>
+        <script>
+		var element = document.getElementById("<?php echo $_SERVER['REQUEST_URI'];?>");
+		if ('<?php echo $_SERVER['REQUEST_URI'];?>' == '/index.php') {
+			document.getElementById("/").classList.add("clicked");
+		} else {
+			element.classList.add("clicked");
+		}
+        </script>
