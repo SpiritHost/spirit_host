50eefdde0ac2c1cde2548bab7ae3bf98392392d1
csf30816 <elijahporter@zoho.com>
2016-09-04 19:49:07 -0500

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,7 @@
+        <div id="head_bar">
+            <ul class="navbar">
+                <a href="/">
+                    <li>Home</li>
+                </a>
+            </ul>
+        </div>
