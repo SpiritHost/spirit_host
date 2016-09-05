d9b745804426b937009c40f974b015b43a0ff575
csf30816 <elijahporter@zoho.com>
2016-09-04 19:49:36 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <link type="text/css" rel="stylesheet" href="style.css">
+        <title>Spirit Host</title>
+    </head>
+
+    <body>
+        <?php include 'navbar.php'; ?>
+    </body>
+
+</html>
