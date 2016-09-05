51bad85d0c741168aa26a30f49498acc648ebbd8
csf30816 <elijahporter@zoho.com>
2016-09-05 10:36:31 -0500

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,7 +1,11 @@
-        <div id="head_bar">
+	<div id="head_bar">
             <ul class="navbar">
                 <a href="/">
                     <li>Home</li>
                 </a>
+                <a href="/signup.php">
+                    <li>Signup</li>
+                </a>
+                <span>&nbsp;</span>
             </ul>
         </div>
