3f52205a2e81041bd68926788efefd47e656cad0
csf30816 <elijahporter@zoho.com>
2016-09-04 19:48:17 -0500

    Create maintenance.php

---
+++ maintenance.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Spirit Host</title>
+        <style>
+        	<?php include 'style.css'; ?>
+        </style>
+    </head>
+
+    <body>
+        <?php include 'navbar.php'; ?>
+        <div class="content">
+            <div class="inner">
+                <center>
+                    <h2 style="margin: 0;">Down For Maintenance</h2>
+                </center>
+                <hr>
+                <center>This site is temporarily down for maintenance. Check back later. This could take any where from a couple hours to a couple weeks. Thanks for your patience!
+                </center>
+            </div>
+        </div>
+    </body>
+
+</html>
